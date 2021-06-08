d8b24210c4575f20caea7c9f8761be39d80f7b2c
arturbipolar <kachokabricosyan@gmail.com>
2021-06-08 21:33:59 +0300

    Добавьте файлы проекта.

---
+++ QuizGenerator/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CreateQuiz(object sender, RoutedEventArgs e)
+        {
+            CreateQuiz f = new CreateQuiz(); f.Show();
+        }
+
+        private void Facts(object sender, RoutedEventArgs e)
+        {
+            Facts f = new Facts(); f.Show();
+        }
+    }
+}
